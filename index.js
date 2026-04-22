e4bc9418ea72c3a6ae08aeed2356fe7c2b76fc5b
sflakes192-glitch <sflakes192@gmail.com>
2026-04-22 09:57:05 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const P = require("pino")
+
+async function startBot() {
+  const { state, saveCreds } = await useMultiFileAuthState("auth")
+
+  const sock = makeWASocket({
+    logger: P({ level: "silent" }),
+    auth: state,
+    printQRInTerminal: false
+  })
+
+  // Pairing code
+  if (!sock.authState.creds.registered) {
+    const number = "2349129517597"
+    const code = await sock.requestPairingCode(number)
+    console.log("PAIRING CODE:", code)
+  }
+
+  sock.ev.on("creds.update", saveCreds)
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+    const m = messages[0]
+    if (!m.message) return
+
+    const sender = m.key.remoteJid
+
+    const text =
+      m.message.conversation ||
+      m.message.extendedTextMessage?.text ||
+      ""
+
+    if (text === ".ping") {
+      await sock.sendMessage(sender, { text: "🏓 Yuki is alive!" }, { quoted: m })
+    }
+  })
+}
+
+startBot()
